cba6704cb1cb355f8e3a3008c4a69ebc993e5c78
shawnfosterfx <shawn.foster79@gmail.com>
2013-11-13 09:32:43 -0500

    Extended Activity and added onCreate Method

--- src/com/engilitycorp/codeathon/TestActivity.java
+++ src/com/engilitycorp/codeathon/TestActivity.java
@@ -1,5 +1,10 @@
 package com.engilitycorp.codeathon;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.lang.Override;
+
 /**
  * Created with IntelliJ IDEA.
  * User: pifko
@@ -7,5 +12,10 @@ package com.engilitycorp.codeathon;
  * Time: 9:26 AM
  * To change this template use File | Settings | File Templates.
  */
-public class TestActivity {
+public class TestActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
