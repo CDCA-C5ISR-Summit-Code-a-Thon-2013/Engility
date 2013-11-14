e45cfd3c56edd17f1b503814c4b220005fee9dbf
KPifko <kpifko@gmail.com>
2013-11-13 20:10:46 -0500

    handle messages

---
+++ src/com/engilitycorp/codeathon/location/TextHandler.java
@@ -0,0 +1,20 @@
+package com.engilitycorp.codeathon.location;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pifko
+ * Date: 11/13/13
+ * Time: 8:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TextHandler  extends Handler {
+
+    public void handleMessage( Message msg ){
+
+    }
+
+
+}
